2cbb312d5f18798bdb26c842f8ca1990148b1978
Godfrey <gtwene001@st.ug.edu.gh>
2021-10-07 18:43:29 +0000

    Styles Added in js file

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Grid } from '@material-ui/core';
 
 import Product from './Product/Product';
+import useStyles from './styles';
 
 const products = [
     { id: 1, name: 'shoes', description: 'Running Shoes.', price: '$5', image: 'https://media.istockphoto.com/photos/creative-minimal-paper-idea-concept-white-shoe-with-white-background-picture-id1149654373?k=20&m=1149654373&s=612x612&w=0&h=YfIwQ1QylFJ2sxMmjKAWLONWniGq9PiMGBkVeBeB9oQ=' },
@@ -9,8 +10,10 @@ const products = [
 ];
 
 const Products = () => {
+    const classes = useStyles();
     return(
-        <main>
+        <main className={classes.content}>
+            <div className={classes.toolbar} />
         <Grid container justify="center" spacing={4}>
             {products.map((product) => (
                 <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>

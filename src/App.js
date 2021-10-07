c5745df9ca053d4b8103f18c9a557489f10d81d7
Godfrey <gtwene001@st.ug.edu.gh>
2021-10-07 22:58:35 +0000

    cart and cartItems added from the api

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from 'react'
 import { commerce } from './lib/commerce'
-import { Products, Navbar } from './components';
+import { Products, Navbar, Cart } from './components';
+import { set } from 'react-hook-form';
 
 
  const App = () => {
      const [products, setProducts] = useState([]);
+     const [cart, setCart] = useState({});
 
      const fetchProducts = async () => {
          const {data} = await commerce.products.list();
@@ -12,15 +14,28 @@ import { Products, Navbar } from './components';
          setProducts(data);
      }
 
+     const fetchCart = async () => {
+         setCart(await commerce.cart.retrieve())
+     }
+
+     const handleAddToCart = async (productId, quantity) => {
+        const item = await commerce.cart.add(productId, quantity);
+
+        setCart(item.cart);
+     }
+
      useEffect(() => {
         fetchProducts();
+        fetchCart();
      }, []);
 
+     console.log(cart)
 
     return (
         <div>
-            <Navbar />
-            <Products products={products} />
+            <Navbar totalItems={cart.total_items} />
+            {/* <Products products={products} onAddToCart={handleAddToCart} /> */}
+            <Cart cart={cart} />
         </div>
     )
 }

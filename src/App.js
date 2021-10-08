cec2d97f5f6f7d12ecb7f52096575dfef0e6404b
Godfrey <gtwene001@st.ug.edu.gh>
2021-10-08 13:33:27 +0000

    functions

--- src/App.js
+++ src/App.js
@@ -17,11 +17,29 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
      const fetchCart = async () => {
          setCart(await commerce.cart.retrieve())
      }
-
+ 
      const handleAddToCart = async (productId, quantity) => {
-        const item = await commerce.cart.add(productId, quantity);
+        const {cart} = await commerce.cart.add(productId, quantity);
 
-        setCart(item.cart);
+        setCart(cart);
+     }
+
+     const handleUpdateCartQty = async (productId, quantity) => {
+         const { cart } = await commerce.cart.update(productId, {quantity})
+
+         setCart(cart)
+     }
+
+     const handleRemoveFromCart = async (productId) => {
+        const { cart } = await commerce.cart.remove(productId);
+
+        setCart(cart)
+     } 
+
+     const handleEmptyCart = async () => {
+         const { cart } = await commerce.cart.empty();
+
+         setCart(cart)
      }
 
      useEffect(() => {
@@ -40,7 +58,12 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
                         <Products products={products} onAddToCart={handleAddToCart} />
                     </Route>
                     <Route exact path="/cart">
-                        <Cart cart={cart} />
+                        <Cart 
+                            cart={cart} 
+                            handleUpdateCartQty={handleUpdateCartQty}
+                            handleRemoveFromCart={handleRemoveFromCart}
+                            handleEmptyCart={handleEmptyCart}
+                         />
                     </Route>
                     
                 </Switch>
